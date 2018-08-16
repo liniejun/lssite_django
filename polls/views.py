9c775256272b42b4e6c85c2de54283426d7ae3de
Leeziyou <18219529882@163.com>
2018-08-16 20:52:14 +0800

    base function realize

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from .models import Question
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Question, Choice
 from django.http import Http404
 from django.shortcuts import get_object_or_404
+from django.urls import reverse
 
 # Create your views here.
 
@@ -23,9 +24,22 @@ def detail(request, question_id):
     return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
-    response = 'you are looking at the results of question %s.'
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
-    return HttpResponse('you are voting on question %s' % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # 发生choice未找到异常时，重新返回表单页面，并给出提示信息
+        return render(request, 'polls/detail.html',{
+            'question': question,
+            'error_message': 'You did not select a choice.',
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # 成功处理数据后，自动跳转到结果页面，防止用户连续多次提交
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
 

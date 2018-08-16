1e4da23546716b5c4fe6644f46bc999591276842
Leeziyou <18219529882@163.com>
2018-08-16 20:00:26 +0800

    use views,use urls,use templates

--- polls/urls.py
+++ polls/urls.py
@@ -11,6 +11,11 @@ __author__ = 'Shawn Lee'
 from . import views
 from django.urls import path
 
+
+app_name = 'polls' # 指定URLconf的命名空间
 urlpatterns =[
     path('', views.index, name='index'),
+    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
